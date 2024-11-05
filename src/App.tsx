e441db924c71459e10f6cbedc5cea975a94addda
rellityuint32 <zachari.iligan@asceoft.com>
2024-11-06 01:09:09 +0800

    initial setup

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+import { Hammer } from "lucide-react"
+
+function App() {
+
+  return (
+    <div className=" flex flex-col items-center h-screen">
+      hello, this is a wip site for rell's portfolio projects
+      <div className="flex flex-row gap-2">
+        <Hammer /> coming soon&trade;
+      </div>
+    </div>
+  )
+}
+
+export default App
