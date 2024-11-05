ad3f21a2eb0472ed7bb64ff895ad989b5c164244
rellityuint32 <zachari.iligan@asceoft.com>
2024-11-06 01:39:34 +0800

    ui essentials setup + themes

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,21 @@
 import { Hammer } from "lucide-react"
+import { ThemeProvider } from "./components/utils/theme-provider"
+import { ModeToggle } from "./components/utils/mode-toggle"
 
 function App() {
-
   return (
-    <div className=" flex flex-col items-center h-screen">
-      hello, this is a wip site for rell's portfolio projects
-      <div className="flex flex-row gap-2">
-        <Hammer /> coming soon&trade;
+    <ThemeProvider defaultTheme="dark" storageKey="vite-ui-theme">
+      <div className="flex flex-col items-center h-screen p-4">
+        hello, this is a wip site for rell's portfolio projects
+        <div className="flex flex-row gap-2">
+          <Hammer /> coming soon&trade;
+        </div>
       </div>
-    </div>
+      <div className="fixed flex flex-row gap-2 bottom-0 left-0 m-4 p-2 bg-background rounded-lg">
+        <ModeToggle />
+      </div>
+
+    </ThemeProvider>
   )
 }
 
